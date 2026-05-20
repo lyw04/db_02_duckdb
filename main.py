5dcda07da98411b45aee50e4edaec998612d15dc
lyw04 <enbee524@gmail.com>
2026-05-20 12:53:48 +0900

    ⚡filtering

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import flet as ft
-# import pandas as pd
 import duckdb
 
 
@@ -7,12 +6,11 @@ def main(page: ft.Page):
     page.title = "Asset List"
     page.padding = 16
     page.window.width = 450
-    page.window.height = 300
-    # page.scroll = ft.ScrollMode.ADAPTIVE
+    page.window.height = 500
 
     # 데이터베이스 접속
     con = duckdb.connect("data/finance.db")
-
+    
     # 테이블 생성
     con.execute("""
         CREATE TABLE IF NOT EXISTS assets (
@@ -37,6 +35,18 @@ def main(page: ft.Page):
     # DB로부터 Pandas DataFrame으로 읽어오기
     df = con.execute("SELECT * FROM assets").df()
 
+    def get_data(filter_query=""):
+        if filter_query:
+            query = f"""
+                SELECT * FROM assets 
+                WHERE name ILIKE '%{filter_query}%' 
+            """
+            return con.execute(query).df()
+        else:
+            return con.execute("SELECT * FROM assets").df()
+
+    df = get_data()
+
     # 너비 비율 계산
     # lengths = {col: int(max(df[col].astype(str).map(len).max(), len(col))) for col in df.columns}
     lengths = {}
@@ -53,26 +63,48 @@ def main(page: ft.Page):
         ],
     )
 
-    # 데이터 리스트
-    data_rows = []
-    for _, row in df.iterrows():
-        data_rows.append(
-            ft.Container(
-                content=ft.Row([
-                    ft.Text(str(row[col]), expand=lengths[col]) for col in df.columns
-                ]),
-                height=40,
+    # 필터링을 반영한 row 생성 함수
+    def create_rows(current_df):
+        rows = []
+        for _, row in current_df.iterrows():
+            rows.append(
+                ft.Container(
+                    content=ft.Row([
+                        ft.Text(str(row[col]), expand=lengths[col]) for col in current_df.columns
+                    ]),
+                    height=40,
+                )
             )
-        )
+        return rows
 
-    # 스크롤되는 영역에 data_rows 지정
+    # 스크롤 되는 영역에 create_rows() 함수 결과 지정
     scrollable_data = ft.Column(
-        controls=data_rows,
+        controls=create_rows(df),
         scroll=ft.ScrollMode.ALWAYS,
         expand=True
     )
 
+    # 필터링 이벤트 핸들러
+    def on_filter_change(e):
+        # 입력된 텍스트로 DB 검색
+        filtered_df = get_data(e.control.value)
+        scrollable_data.controls = create_rows(filtered_df)
+        page.update()
+
+    # 필터 입력
+    filter_input = ft.Container(
+        ft.TextField(
+            label="종목 검색",
+            prefix_icon=ft.Icons.SEARCH,
+            hint_text="종목명을 입력하세요",
+            hint_style=ft.TextStyle(color=ft.Colors.GREY_700),
+            expand=True,
+            on_change=on_filter_change,
+        )
+    )
+
     page.add(
+        filter_input,
         sticky_header,
         ft.Divider(),
         scrollable_data
