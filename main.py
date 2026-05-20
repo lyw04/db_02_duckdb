1efc3a8fb0925542f1e7669018f1a288157f975f
lyw04 <enbee524@gmail.com>
2026-05-20 12:45:04 +0900

    🗃️Primary Key 추가

--- main.py
+++ main.py
@@ -6,18 +6,35 @@ import duckdb
 def main(page: ft.Page):
     page.title = "Asset List"
     page.padding = 16
+    page.window.width = 400
+    page.window.height = 400
 
     # 데이터베이스 접속
     con = duckdb.connect("data/finance.db")
 
-    # assets.csv 파일을 읽어서 테이블 생성
+    # 테이블 생성
     con.execute("""
-        CREATE TABLE IF NOT EXISTS assets
-        AS SELECT * 
-        FROM read_csv_auto('data/assets.csv')
+        CREATE TABLE IF NOT EXISTS assets (
+            ticker VARCHAR PRIMARY KEY,
+            name VARCHAR,
+            type VARCHAR
+        )
     """)
 
-    print('데이터베이스 저장 완료')
+    # assets.csv 파일을 읽어서 테이블에 추가
+    con.execute("""
+        INSERT OR IGNORE INTO assets 
+        SELECT * FROM read_csv_auto('data/assets.csv')
+    """)
+
+    print("데이터베이스 저장 완료")
+    
+    snack_bar = ft.SnackBar(
+        content=ft.Text("데이터베이스 저장 완료")
+    )
+    page.overlay.append(snack_bar)
+    snack_bar.open = True
+
 
 if __name__ == "__main__":
     ft.run(main)
