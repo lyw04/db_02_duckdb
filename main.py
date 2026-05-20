b2d5c89139a775c4eadc4f4faf05f8ca986d7a08
lyw04 <enbee524@gmail.com>
2026-05-20 12:53:05 +0900

    ✨sticky header

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(page: ft.Page):
     page.padding = 16
     page.window.width = 450
     page.window.height = 300
-    page.scroll = ft.ScrollMode.ADAPTIVE
+    # page.scroll = ft.ScrollMode.ADAPTIVE
 
     # 데이터베이스 접속
     con = duckdb.connect("data/finance.db")
@@ -37,16 +37,46 @@ def main(page: ft.Page):
     # DB로부터 Pandas DataFrame으로 읽어오기
     df = con.execute("SELECT * FROM assets").df()
 
-    # DataTable 생성
-    data_table = ft.DataTable(
-        columns=[ft.DataColumn(ft.Text(col.upper())) for col in df.columns],
-        rows=[
-            ft.DataRow(cells=[ft.DataCell(ft.Text(str(val))) for val in row])
-            for row in df.values
+    # 너비 비율 계산
+    # lengths = {col: int(max(df[col].astype(str).map(len).max(), len(col))) for col in df.columns}
+    lengths = {}
+    for col in df.columns:
+        # 컬럼명 자체의 길이와 데이터들 중 최대 길이
+        max_len = max(df[col].astype(str).map(len).max(), len(col))
+        lengths[col] = int(max_len)
+
+    # 헤더
+    sticky_header = ft.Row(
+        controls=[
+            ft.Container(ft.Text(col.upper(), weight="bold", size=16), expand=lengths[col]) 
+            for col in df.columns
         ],
     )
 
-    page.add(data_table)
+    # 데이터 리스트
+    data_rows = []
+    for _, row in df.iterrows():
+        data_rows.append(
+            ft.Container(
+                content=ft.Row([
+                    ft.Text(str(row[col]), expand=lengths[col]) for col in df.columns
+                ]),
+                height=40,
+            )
+        )
+
+    # 스크롤되는 영역에 data_rows 지정
+    scrollable_data = ft.Column(
+        controls=data_rows,
+        scroll=ft.ScrollMode.ALWAYS,
+        expand=True
+    )
+
+    page.add(
+        sticky_header,
+        ft.Divider(),
+        scrollable_data
+    )
 
 
 if __name__ == "__main__":
