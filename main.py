f08797c447b4e63679b57d7c4a0538ba8f599a45
lyw04 <enbee524@gmail.com>
2026-05-20 12:56:22 +0900

    💄place holder

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import flet as ft
 import duckdb
+# import random
 
 
 def main(page: ft.Page):
     page.title = "Asset List"
     page.padding = 16
     page.window.width = 450
-    page.window.height = 500
+    page.window.height = 530
 
     # 데이터베이스 접속
     con = duckdb.connect("data/finance.db")
@@ -60,8 +61,12 @@ def main(page: ft.Page):
     # 헤더
     sticky_header = ft.Row(
         controls=[
-            ft.Container(ft.Text(col.upper(), weight="bold", size=16), expand=lengths[col]) 
-            for col in df.columns
+            ft.Container(width=40),
+            *[ft.Container(
+                ft.Text(col.upper(), weight="bold", size=16), 
+                expand=lengths[col], 
+            ) 
+            for col in df.columns]
         ],
     )
 
@@ -69,10 +74,44 @@ def main(page: ft.Page):
     def create_rows(current_df):
         rows = []
         for _, row in current_df.iterrows():
+            # 배경색 랜덤 생성
+
+            # # hex_color = "".join(f"{random.randint(60, 255):02x}" for _ in range(3))
+            # r = random.randint(60, 255)
+            # g = random.randint(60, 255)
+            # b = random.randint(60, 255)
+            # hex_color = f"{r:02x}{g:02x}{b:02x}"
+
+            # 이름의 각 글자 유니코드 값을 더해서 고유 숫자 생성
+            name_sum = sum(ord(char) for char in str(row['name']))
+            
+            # Red, Green, Blue 값 (0~255) 결정
+            # 각 값에 다른 소수를 곱해서 색상이 겹치지 않도록 해줌
+            # 196으로 나눈 나머지(0~195)에 60을 더하면 최대 255 (0xff)
+            r = (name_sum * 13) % 196 + 60
+            g = (name_sum * 17) % 196 + 60
+            b = (name_sum * 19) % 196 + 60
+            
+            hex_color = f"{r:02x}{g:02x}{b:02x}"
+            
+            img_column = ft.Container(
+                content=ft.Image(
+                    # 종목명 앞에서부터 5글자를 사용해서 place holder 이미지 요청
+                    src=f"https://placehold.jp/30/{hex_color}/000/150x150.png?text={row['name'][:5]}",
+                    width=30,
+                    height=30,
+                ),
+                width=30,
+                height=30,
+                border_radius=15, # 반지름을 줘서 원형 마스크 생성
+                clip_behavior=ft.ClipBehavior.ANTI_ALIAS,
+            )
+
             rows.append(
                 ft.Container(
                     content=ft.Row([
-                        ft.Text(str(row[col]), expand=lengths[col]) for col in current_df.columns
+                        ft.Container(img_column, width=40, alignment=ft.Alignment.CENTER),
+                        *[ft.Text(str(row[col]), expand=lengths[col]) for col in current_df.columns]
                     ]),
                     height=40,
                 )
