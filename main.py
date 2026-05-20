bea755bbd04e5b256ca068b5725807bec7c0022d
lyw04 <enbee524@gmail.com>
2026-05-20 12:49:51 +0900

    ✨DataTable

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import duckdb
 def main(page: ft.Page):
     page.title = "Asset List"
     page.padding = 16
-    page.window.width = 400
-    page.window.height = 400
+    page.window.width = 450
+    page.window.height = 500
 
     # 데이터베이스 접속
     con = duckdb.connect("data/finance.db")
@@ -34,7 +34,33 @@ def main(page: ft.Page):
     )
     page.overlay.append(snack_bar)
     snack_bar.open = True
+ 
+    # DB로부터 Pandas DataFrame으로 읽어오기
+    df = con.execute("SELECT * FROM assets").df()
 
+    # DataTable 생성
+    data_table = ft.DataTable(
+        columns=[
+            ft.DataColumn(ft.Text("티커")),
+            ft.DataColumn(ft.Text("종목명")),
+            ft.DataColumn(ft.Text("종류")),
+        ],
+        rows=[]
+    )
+
+    # DataRow로 변환하여 추가
+    for _, row in df.iterrows():
+        data_table.rows.append(
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(row['ticker'])),
+                    ft.DataCell(ft.Text(row['name'])),
+                    ft.DataCell(ft.Text(row['type'])),
+                ]
+            )
+        )
+
+    page.add(data_table)
 
 if __name__ == "__main__":
     ft.run(main)
